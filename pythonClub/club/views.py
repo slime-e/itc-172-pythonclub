259073fe0d19cec42d2ba39a17857a5dc5e27689
slime-e <63755019+slime-e@users.noreply.github.com>
2020-06-02 14:33:50 -0700

    assignment_5
    changes to the views.py and urls.py, as well as adding types.html

--- pythonClub/club/views.py
+++ pythonClub/club/views.py
@@ -1,5 +1,13 @@
+from .models import ProductType, Product, Review
 from django.shortcuts import render
 
 # Create your views here.
 def index (request):
     return render(request, 'club/index.html')
+
+
+def gettypes(request):
+    type_list=ProductType.objects.all()
+    return render(request, 'club/types.html' ,{'type_list' : type_list})
+
+

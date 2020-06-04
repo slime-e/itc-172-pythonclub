ba4091c8993a49ec4e89916d0d24a8b8dc16945d
slime-e <63755019+slime-e@users.noreply.github.com>
2020-06-03 17:40:47 -0700

    assignment_6
    new views for the details and products pages, added to urls and their own templates

--- pythonClub/club/views.py
+++ pythonClub/club/views.py
@@ -1,5 +1,5 @@
 from .models import ProductType, Product, Review
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
 def index (request):
@@ -11,3 +11,18 @@ def gettypes(request):
     return render(request, 'club/types.html' ,{'type_list' : type_list})
 
 
+def getproducts(request):
+    products_list=Product.objects.all()
+    return render(request, 'club/products.html', {'products_list': products_list})
+
+
+def productdetails(request, id):
+    prod = get_object_or_404(Product, pk=id)
+    discount=prod.memberdiscount
+    reviews = Review.objects.filter(product=id).count()
+    context={
+        'prod' : prod,
+        'discount' : discount,
+        'reviews' : reviews,
+    }
+    return render(request, 'club/proddetails.html', context=context)
